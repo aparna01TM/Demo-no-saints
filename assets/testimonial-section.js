9a1c3cd78af1979964a54ece1dcc0892231e730e
aparna01TM <aparna.hari@terrificminds.com>
2023-10-09 14:42:31 +0530

    pushing changes

--- assets/testimonial-section.js
+++ assets/testimonial-section.js
@@ -4,32 +4,32 @@ $('.section-containerr').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow: $('#left-arrow'),
-    nextArrow: $('#right-arrow')
+    nextArrow: $('#right-arrow'),
 
-    // responsive: [
-    //     {
-    //       breakpoint: 1250,
-    //       settings: {
-    //         slidesToShow: 3,
-    //         slidesToScroll: 1,
-    //         infinite: true,
-    //         dots: true
-    //       }
-    //     },
-    //     {
-    //       breakpoint: 990,
-    //       settings: {
-    //         slidesToShow: 2,
-    //         slidesToScroll: 1
-    //       }
-    //     },
-    //     {
-    //       breakpoint: 670,
-    //       settings: {
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1
-    //       }
-    //     }
-    //   ]
+    responsive: [
+        {
+          breakpoint: 1250,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 990,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 670,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
    });
 });
